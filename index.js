1ebcc769e86408de2d127a25568249f88a531c18
zuleika <njokizuleika@gmail.com>
2023-04-17 13:05:15 +0300

    done has target sum lab

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+
+for(let i = 0; i < array.length; i++) {
+  for(let j = i + 1; j < array.length; j++) {
+    if(array[i] + array[j] === target) {
+      return true;
+    }
+  }
 }
+return false;
+}
+
 
 /* 
   Write the Big O time complexity of your function here
